544e516940188c65c71e71742db387ef1e6720bd
Oana Vasilescu <vasilescu.oana28@gmail.com>
2023-07-04 00:04:15 +0300

    added Vivi's alarm scenario

--- app/risk-app/webapp/controller/AlarmScenarios.controller.js
+++ app/risk-app/webapp/controller/AlarmScenarios.controller.js
@@ -1,12 +1,34 @@
 sap.ui.define([
     "riskapp/controller/BaseController",
     "sap/ui/model/json/JSONModel",
-    "riskapp/utils/URLs"
-], function (BaseController, JSONModel, URLs) {
+    "riskapp/utils/URLs",
+    "sap/ui/core/Fragment"
+], function (BaseController, JSONModel, URLs, Fragment) {
     "use strict";
 
     return BaseController.extend("riskapp.controller.AlarmScenarios", {
         onInit() {
         },
+
+        openScenarioDialog: function () {
+            let view = this.getView();
+            if (!this._lPop) {
+                this._lPop = Fragment.load({
+                    id: view.getId(),
+                    name: "riskapp.view.fragments.AddAlarmScenario",
+                    controller: this
+                }).then((mPop) => {
+                    this.getView().addDependent(mPop);
+                    return mPop;
+                });
+            }
+            this._lPop.then(function (mPop) {
+                mPop.open();
+            })
+        },
+
+        handleCancelAddScenario: function () {
+            this.byId("addAlarm").close();
+        }
     });
 });
