e29f949bd900463a8f3275e0e41a5ad12fc19522
Oana Vasilescu <vasilescu.oana28@gmail.com>
2023-07-03 23:52:41 +0300

    fixed preflight access control

--- gen/srv/srv/server.js
+++ gen/srv/srv/server.js
@@ -8,6 +8,13 @@ cds.on('bootstrap', async app => {
         res.setHeader('access-control-allow-origin', '*')
         res.set('access-control-allow-origin', '*')
 
+        if (req.method === 'OPTIONS') {
+            res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+            res.set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+            return res.set('access-control-allow-methods', 'GET,HEAD,PUT,PATCH,POST,DELETE').end()
+        }
+
         next()
     })
 })
