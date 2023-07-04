caa7de9d2bc7c02f5c77662e513e01406671041f
Diana Gornic <diana.gornic@sap.com>
2023-07-04 16:07:09 +0300

    Alarm scenarios

--- app/risk-app/webapp/controller/AlarmScenarios.controller.js
+++ app/risk-app/webapp/controller/AlarmScenarios.controller.js
@@ -8,26 +8,40 @@ sap.ui.define([
 
     return BaseController.extend("riskapp.controller.AlarmScenarios", {
         onInit() {
+          let alarmModel = new JSONModel();
+          this.getView().setModel(alarmModel, "alarmModel");
         },
 
-        openScenarioDialog: function () {
+        openScenarioDialog: function() {
             let view = this.getView();
             if (!this._lPop) {
                 this._lPop = Fragment.load({
-                    id: view.getId(),
-                    name: "riskapp.view.fragments.AddAlarmScenario",
-                    controller: this
+                  id: view.getId(),
+                  name: "riskapp.view.fragments.AddAlarmScenario",
+                  controller: this
                 }).then((mPop) => {
-                    this.getView().addDependent(mPop);
-                    return mPop;
+                  this.getView().addDependent(mPop);
+                  return mPop;
                 });
-            }
-            this._lPop.then(function (mPop) {
+              }
+              this._lPop.then(function (mPop) {
                 mPop.open();
-            })
+              })
         },
 
-        handleCancelAddScenario: function () {
+        handleYesAddScenario: async function() {
+          const oData = this.getView().getModel("alarmModel").getData();
+          console.log(oData);
+
+          await this.post("/app/AlarmScenarios", oData).then(async (data) => {
+            this.messageHandler("Alarm scenario has been created");
+          }).catch((err) => {
+              this.messageHandler("uploadRiskEventError");
+              return "error";
+          });
+        },
+
+        handleCancelAddScenario: function() {
             this.byId("addAlarm").close();
         }
     });
